6703e420190a00a799bd537b1350f35a07288881
Joseph Burton <josephburton06@gmail.com>
2019-07-17 15:13:47 -0500

    Add more helpers

--- sms_helpers.py
+++ sms_helpers.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 import numpy as np 
 
-def original_word_count():
+import unicodedata
+import re
+import json
+
+def original_word_count(df):
     '''This function adds a new column to the dataframe which counts the number of words in the README file'''
     df[['original_cnt']] = df[['original']]
     art_nums = list(range(df.original.count()))
@@ -10,4 +14,45 @@ def original_word_count():
     df['original_cnt']=df['original_cnt'].astype(int)
     return df
 
+def basic_clean(df):
+    '''This function adds a new column to the dataframe which takes the original README file text and does some cleaning.
+    It lower cases, normalizes, encodes and decodes with ascii and utf-8 decoding, then removes special character. 
+    Returns the clean text in the new column, article'''
+    df[['article']] = df[['original']]
+    art_nums = list(range(df.original.count()))
+    for art_num in art_nums:
+        original = df.article[art_num]
+        article = re.sub(r'\s', ' ', original).lower()
+        normalized = unicodedata.normalize('NFKD', article)
+        just_ascii = normalized.encode('ascii', 'ignore')
+        article = just_ascii.decode('utf-8')
+        without_special_chars = re.sub(r'[^\w\s]', ' ', article)
+        word_list = without_special_chars.split()
+        article_final = ' '.join(word_list)
+        df.article[art_num] = article_final
+    return df
+
+def article_word_count(df):
+    '''This function adds a new column to the dataframe and does a word count of the article column.'''
+    df[['article_cnt']] = df[['article']]
+    art_nums = list(range(df.original.count()))
+    for art_num in art_nums:
+        df.article_cnt[art_num] = len(df['article'][art_num].split())
+    df['article_cnt']=df['article_cnt'].astype(int)
+    return df
     
+def article_percent(df):
+    '''This function calculates what percentage of the original text was kept after the basic clean function.'''
+    df[['article_per_kept']] = df[['article_cnt']]
+    df[['article_per_kept']] = df.article_cnt / df.original_cnt
+    df['article_per_kept']=df['article_per_kept'].astype(float)
+    return df
+
+def text_prep(df):
+
+    df = original_word_count(df)
+    df = basic_clean(df)
+    df = article_word_count(df)
+    df = article_percent(df)
+
+    return df
